1da77a0d01f7a047b6b5663899b7e5ceb5056979
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-01-22 18:50:08 +0530

    added conditional styling to the navbar based on the current page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
+
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
   return (
     <div>
         <div className={styles.navbar}>
-            <Link className={styles.links} to='/'>Home</Link>
-            <Link className={styles.links} to='/about'>About</Link>
-            <Link className={styles.links} to='/experience'>Work Experience</Link>
-            <Link className={styles.links} to='/projects'>Projects</Link>
+            <Link className={isActive('/') ? `${styles.active} ${styles.links}` : styles.links} to='/'>Home</Link>
+            <Link className={isActive('/about') ? `${styles.active} ${styles.links}` : styles.links} to='/about'>About</Link>
+            <Link className={isActive('/experience') ?  `${styles.active} ${styles.links}` : styles.links} to='/experience'>Work Experience</Link>
+            <Link className={isActive('/projects') ? `${styles.active} ${styles.links}` : styles.links} to='/projects'>Projects</Link>
         </div>
     </div>
   )
