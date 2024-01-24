198edd94f9963c53a23192c2c45f7ea9d3add785
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-01-24 23:13:38 +0530

    added a loading message tothe projects page while they are being mapped to the dom and added a place for errors in case documents fail to load

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -4,13 +4,15 @@ import { useCollection } from '../../hooks/useCollection';
 
 export default function Projects() {
 
-  const { documents } = useCollection('projects');
+  const { documents, error } = useCollection('projects');
 
   return (
     <div className={styles['projects-container']}>
+        {!documents && <p>Loading projects...</p>}
         {documents && documents.map(project => (
           <ProjectCard key={project.id} props={project}/>
         ))}
+        {error && <p>{error}</p>}
     </div>
   )
 };
