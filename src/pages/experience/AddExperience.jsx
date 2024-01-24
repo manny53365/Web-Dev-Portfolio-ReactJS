f583738a61da68479d73904484ae5e3609c18d3f
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-01-24 15:47:01 +0530

    added code for the add project page

--- src/pages/experience/AddExperience.jsx
+++ src/pages/experience/AddExperience.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function AddExperience() {
   return (
     <div>
-        AddExperience
-        </div>
+      AddExperience
+    </div>
   )
 };
--- src/pages/projects/AddProject.jsx
+++ src/pages/projects/AddProject.jsx
@@ -1,9 +1,69 @@
-import React from 'react';
+import { useState } from 'react';
+import { Box, TextField, Button, Input, InputLabel } from '@mui/material';
+
+import styles from './AddProject.module.css';
 
 export default function AddProject() {
+
+  const [projectName, setProjectName] = useState('');
+  const [description, setDescription] = useState('');
+  const [siteLink, setSiteLink] = useState('');
+  const [repoLink, setRepoLink] = useState('');
+  const [projectThumbnail, setProjectThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState('');
+
+  const handleFileChange = event => {
+    setProjectThumbnail(null);
+    let selectedImg = event.target.files[0];
+    console.log(selectedImg);
+
+    if (!selectedImg){
+        setThumbnailError('Please select a file');
+        return;
+    };
+    if (!selectedImg.type.includes('image')){
+        setThumbnailError('Selected file must be an image');
+        return;
+    };
+    if (selectedImg.size > 100000){
+        setThumbnailError('Image file size must be less than 100kb');
+        return;
+    };
+
+    setThumbnailError(null);
+    setProjectThumbnail(selectedImg);
+    console.log('Thumbnail updated')
+};
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // await login(email, password);
+    // navigate('/');
+};
+
   return (
-    <div>
-        AddProject
-        </div>
+    <Box onSubmit={handleSubmit} className={styles['add-project-form']} component="form" sx={{'& > :not(style)': { m: 1, width: '25ch' },}} noValidate autoComplete="off">
+        <h2>Add Project - Admin Panel</h2>
+        <TextField label="Project Name" variant="outlined" type='text' required={true} value={projectName} 
+        onChange={event => setProjectName(event.target.value)}
+        />
+        <TextField label="Description" multiline type='text' required={true} value={description} rows={4}
+        onChange={event => setDescription(event.target.value)}
+        />
+        <TextField label="Live website link" variant="outlined" type='text' required={true} value={siteLink}
+        onChange={event => setSiteLink(event.target.value)}
+        />
+        <TextField label="Github repo link" variant="outlined" type='text' value={repoLink}
+        onChange={event => setRepoLink(event.target.value)}
+        />
+        <InputLabel>Upload your project thumbnail:</InputLabel>
+        <Input type='file' variant='outlined' required={true} value={projectThumbnail}  accept="image/*"
+          onChange={handleFileChange} 
+        />
+        {thumbnailError && <div className='error'>{thumbnailError}</div>}
+        {/* {<Button variant="outlined" type='submit' disabled>Logging in...</Button>} */}
+        {<Button variant="outlined" type='submit'>Log in</Button>}
+        {/* {error && <p>{error}</p>} */}
+    </Box>
   )
 };
