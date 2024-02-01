3d5221f98911cc52982082cf2703bf6136bdf77a
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-02-01 15:44:21 +0530

    set the intro text to be bullet points instead of a large paragraph

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div className={styles.intro}>
           <img className={styles['profile-image']} src='https://firebasestorage.googleapis.com/v0/b/web-dev-portfolio-6b1e9.appspot.com/o/photos%2FManuel_Rodriguez.png?alt=media&token=492b6b56-b0a4-41d3-8834-782d7eda08bd' alt="author" />
             <h2>Intro</h2>
-            <p className={styles['intro-text']}>
+            {/* <p className={styles['intro-text']}>
             Experienced Software Engineer with a proven track record in developing, maintaining, and enhancing robust applications.
             Highly skilled in addressing and resolving client-reported issues efficiently.
             Demonstrated expertise in implementing innovative features and optimizing existing systems using technologies like Python, MongoDB, and React.
@@ -18,7 +18,30 @@ export default function Home() {
             Known for effective collaboration with cross-functional teams and clients, ensuring applications are in sync with evolving business requirements.
             Committed to continuous learning and applying the latest technologies to solve real-world challenges.
             Brings a passion for tech-driven solutions, a dedication to delivering exceptional results, and a strong leadership acumen that drives team success.
-            </p>
+            </p> */}
+            <ul>
+              <li className={styles["intro-text"]}>
+              Experienced Software Engineer with a proven track record in developing, maintaining, and enhancing robust applications.
+              </li>
+              <li className={styles["intro-text"]}>
+              Highly skilled in addressing and resolving client-reported issues efficiently.
+              </li>
+              <li className={styles["intro-text"]}>
+              Demonstrated expertise in implementing innovative features and optimizing existing systems using technologies like Python, MongoDB, and React.
+              </li>
+              <li className={styles["intro-text"]}>
+              As a seasoned leader, I have effectively led teams and mentored junior engineers, fostering a collaborative environment and enhancing team performance.
+              </li>
+              <li className={styles["intro-text"]}>
+              Known for effective collaboration with cross-functional teams and clients, ensuring applications are in sync with evolving business requirements.
+              </li>
+              <li className={styles["intro-text"]}>
+              Committed to continuous learning and applying the latest technologies to solve real-world challenges.
+              </li>
+              <li className={styles["intro-text"]}>
+              Brings a passion for tech-driven solutions, a dedication to delivering exceptional results, and a strong leadership acumen that drives team success.
+              </li>
+            </ul>
         </div>
     </div>
   )
